3466048f302f6e89f2a88368ea24e4098aea720c
AmyN226 <anguyen226@student.hbuhsd.edu>
2025-10-23 21:46:30 +0000

    finished

--- Main.java
+++ Main.java
@@ -4,6 +4,37 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    //Problem 1
+
+    System.out.println("Problem 1");
+    Scanner sc = new Scanner(System.in);
+    String word = sc.nextLine();
+    int maxCount = 0;
+
+    
+    String maxLetter = "";
+
+    for (int a = 0; a < word.length(); a++)
+    {
+      String target = word.substring(a, a+1);
+      int counter = 0;
+      for (int b = 0; b < word.length(); b++)
+      {
+        String currentLetter = word.substring(b, b +1);
+        if (currentLetter.equals(target))
+        {
+          counter++;
+        }
+      }
+      
+      if (counter > maxCount)
+      {
+        maxCount = counter;
+        maxLetter = target;
+      }
+
+    }
+    System.out.println("The letter " + maxLetter + " appears the most times at " + maxCount + " times.");
+    sc.close();
   }
 }
